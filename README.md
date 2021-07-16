a82a2ab5302bd41062105d536bc1ad1ee4347d89
Storulv <jordycorvisiercontact@gmail.com>
2021-07-16 09:35:31 +0200

    js for index page

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 05_10062021
-P5 Developpeur Web openclassrooms
+Projet 5 de la formation Développeur Web avec OpenClassrooms.
+
+Pour accéder au projet il suffit de le cloner via l'adresse suivante : https://github.com/Storulv/05_10062021
+
+Afin de faire fonctionner le front vous aurez besoin de Node ainsi que de NPM d'installés localement sur votre ordinateur.
+
+Afin de lancer le serveur exécutez la commande :
+
+    node server
+
+Le serveur devrait se lancer par défaut sur le port :"localhost:3000". Si le serveur se lance sur un autre port celui-ci sera directement indiqué dans la console.
---
+++ public/js/produits.js
@@ -0,0 +1,62 @@
+class AllProducts {
+    constructor() {}
+
+    // Permet de récupérer les produits de l'API et de les afficher
+
+    productsData() {
+        fetch("http://localhost:3000/api/furniture")
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error("ERROR");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                const productHtml = data
+                    .map((furniture) => {
+                        return `
+                    <figure id="figure-produit">
+                        <img src="${
+                            furniture.imageUrl
+                        }" alt="${
+                            furniture.name
+                        }" id="product-image">
+                        <figcaption>
+                            <div>
+                                <h2>
+                                    ${furniture.name}
+                                </h2>
+                            </div>
+                            <div id="" class="">
+                                <p>
+                                    ${furniture.description}
+                                </p>
+                                <div>
+                                    <h3 class="text-xl font-medium merriweather">
+                                        ${furniture.price / 100} €
+                                    </h3>
+                                    <div>
+                                        <a href="../views/produit.html?id${
+                                            furniture._id
+                                        }"> 
+                                            <span>Voir le produit</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </figcaption>
+                    </figure>
+                    `;
+                    })
+                    .join("");
+                document.querySelector("#produits").insertAdjacentHTML("afterbegin", productHtml);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+}
+
+let produits = new AllProducts();
+produits.productsData();
